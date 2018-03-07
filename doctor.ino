32aa7cee8dfa3cb21155b8da88987f5a34744588
chelseycampillo <35706456+chelseycampillo@users.noreply.github.com>
2018-03-07 00:12:12 -0800

    Update doctor.ino

--- doctor.ino
+++ doctor.ino
@@ -1,1 +1,19 @@
 
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  int n = 0;
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  int n = 0;
+  if (n < 5) {
+    digitalWrite(LED_BUILTIN, HIGH);   
+    delay(1000);                      
+    digitalWrite(LED_BUILTIN, LOW);    
+    delay(1000);  
+    n = n + 1;
+  }else {
+    digitalWrite(LED_BUILTIN, LOW);                    
+  }
+}
