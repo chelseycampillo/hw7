980babfc87283ae2e6e9c6a921dcd6a99bb52359
chelseycampillo <35706456+chelseycampillo@users.noreply.github.com>
2018-03-07 00:07:25 -0800

    Update mothers-love.ino

--- mothers-love.ino
+++ mothers-love.ino
@@ -1,1 +1,31 @@
 
+const int ledPin =  6;// the number of the LED pin
+
+
+int ledState = LOW;           
+
+unsigned long previousMillis = 0;       
+
+const long interval = 1;           // interval at which to blink (milliseconds)
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+    digitalWrite(ledPin, ledState);
+  }
+}
