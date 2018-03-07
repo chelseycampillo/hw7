bb2ac57981ebb6b9446189239e746bbf14397657
chelseycampillo <35706456+chelseycampillo@users.noreply.github.com>
2018-03-07 00:14:27 -0800

    Update wrong-turn.ino

--- wrong-turn.ino
+++ wrong-turn.ino
@@ -1,1 +1,15 @@
 
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+    digitalWrite(LED_BUILTIN, HIGH);   
+    delay(100);                      
+    digitalWrite(LED_BUILTIN, LOW);    
+    delay(1000);  
+    digitalWrite(LED_BUILTIN,HIGH);
+    delay(1000);
+
+}
