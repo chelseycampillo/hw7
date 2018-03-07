e5b35d4b7782c4ead63ba3f429a5bad4a28337e0
chelseycampillo <35706456+chelseycampillo@users.noreply.github.com>
2018-03-07 00:03:36 -0800

    Update level-meter.ino

--- level-meter.ino
+++ level-meter.ino
@@ -1,1 +1,60 @@
+/*
+  Analog Input
 
+  Demonstrates analog input by reading an analog sensor on analog pin 0 and
+  turning on and off a light emitting diode(LED) connected to digital pin 13.
+  The amount of time the LED will be on and off depends on the value obtained
+  by analogRead().
+
+  The circuit:
+  - potentiometer
+    center pin of the potentiometer to the analog input 0
+    one side pin (either one) to ground
+    the other side pin to +5V
+  - LED
+    anode (long leg) attached to digital output 13
+    cathode (short leg) attached to ground
+
+  - Note: because most Arduinos have a built-in LED attached to pin 13 on the
+    board, the LED is optional.
+
+  created by David Cuartielles
+  modified 30 Aug 2011
+  By Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/AnalogInput
+*/
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPin5 = 5; // select the pin for the LED
+int ledPin6 = 6;
+int ledPin9 = 9;
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin5, OUTPUT);
+  pinMode(ledPin6, OUTPUT);
+  pinMode(ledPin9, OUTPUT);
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  // turn the ledPin on
+  if (sensorValue == 1/4) {
+    digitalWrite(ledPin5, HIGH);
+  }
+  if (sensorValue == 1/2) {
+  digitalWrite(ledPin6, HIGH);
+  }
+  if (sensorValue == 3/4) {
+  digitalWrite(ledPin9, HIGH);
+  }
+  // turn the ledPin off:
+  digitalWrite(ledPin5, LOW);
+  digitalWrite(ledPin6, LOW);
+  digitalWrite(ledPin9, LOW);
+}
