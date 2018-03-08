155bfd2d15bf0d2034484d4da20ff067c3b0f9f0
chelseycampillo <35706456+chelseycampillo@users.noreply.github.com>
2018-03-07 19:28:16 -0800

    Update level-meter.ino

--- level-meter.ino
+++ level-meter.ino
@@ -1,6 +1,6 @@
 int sensorPin = A0;    // select the input pin for the potentiometer
-int ledPin5 = 5; // select the pin for the LED
-int ledPin6 = 6;
+int ledPin5 = 10; // select the pin for the LED
+int ledPin6 = 11;
 int ledPin9 = 9;
 int sensorValue = 0;  // variable to store the value coming from the sensor
 
@@ -15,17 +15,19 @@ void loop() {
   // read the value from the sensor:
   sensorValue = analogRead(sensorPin);
   // turn the ledPin on
-  if (sensorValue == 1/4) {
+  if (sensorValue > 100) {
     digitalWrite(ledPin5, HIGH);
+  } else {
+    digitalWrite(ledPin5, LOW);
   }
-  if (sensorValue == 1/2) {
+  if (sensorValue > 200) {
   digitalWrite(ledPin6, HIGH);
+  } else {
+    digitalWrite(ledPin6, LOW);
   }
-  if (sensorValue == 3/4) {
+  if (sensorValue > 300) {
   digitalWrite(ledPin9, HIGH);
+  } else {
+    digitalWrite(ledPin9, LOW);
   }
-  // turn the ledPin off:
-  digitalWrite(ledPin5, LOW);
-  digitalWrite(ledPin6, LOW);
-  digitalWrite(ledPin9, LOW);
 }
