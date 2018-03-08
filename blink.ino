0e1208bb94b50edc38124652057caebaa7997f85
chelseycampillo <35706456+chelseycampillo@users.noreply.github.com>
2018-03-07 19:32:30 -0800

    Create blink.ino

---
+++ blink.ino
@@ -0,0 +1,37 @@
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPin5 = 10; // select the pin for the LED
+int ledPin6 = 11;
+int ledPin9 = 9;
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin5, OUTPUT);
+  pinMode(ledPin6, OUTPUT);
+  pinMode(ledPin9, OUTPUT);
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  // turn the ledPin on
+  if (sensorValue > 100) {
+    digitalWrite(ledPin5, HIGH);
+  } else {
+    digitalWrite(ledPin5, LOW);
+  }
+  if (sensorValue > 200) {
+  digitalWrite(ledPin6, HIGH);
+  } else {
+    digitalWrite(ledPin6, LOW);
+  }
+  //blink topmost LED
+  if (sensorValue > 300) {
+  digitalWrite(ledPin9, HIGH);
+  delay(1000);
+  digitalWrite(ledPin9, LOW);
+  delay(1000);
+  } else {
+    digitalWrite(ledPin9, LOW);
+  }
+}
